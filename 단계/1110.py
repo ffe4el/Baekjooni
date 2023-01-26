5cf33a171682a582150594376c18bce5aa39b18e
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-01-26 18:40:20 +0900

    밥 먹으러 가야지...

--- 단계/1110.py
+++ 단계/1110.py
@@ -1,24 +1,8 @@
-<<<<<<< HEAD
-a= list(map(int,input().split()))
-li=[]
+tc = int(input())
+for k in range(tc):
+    n, c = input().split()
+    for i in range(len(c)):
+        for j in range(int(n)):
+            print(c[i], end='')
 
-for i in range(1,a[0]):
-    if a[i] > sum(a)/len(a) :
-        li.append(a[i])
-print(f"{((len(a)-1)/n*100):.3f}%")
-=======
-# 3052
 
-n = [input() in range(10)]
-n=[]
-m=[]
-
-for i in range(10):
-    # int(input())
-    k = int(n[i]) % 10
-    if k not in m:
-        m.append(k)
-
-# print(m)
-print(len(m))
->>>>>>> f0dce50520b4e0955a68c470a77626645f1e6902
