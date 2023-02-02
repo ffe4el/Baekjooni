73dd0e65d53bb390fc60074dc63a84f0aafb4c99
SOLA <93892724+ffe4el@users.noreply.github.com>
2023-02-02 13:58:13 +0900

    ㅇㅇ

--- 단계/1110.py
+++ 단계/1110.py
@@ -1,8 +1,13 @@
-tc = int(input())
-for k in range(tc):
-    n, c = input().split()
-    for i in range(len(c)):
-        for j in range(int(n)):
-            print(c[i], end='')
+# 10989 수 정렬3 이건 1억까지... 시간초과가 문제일것 같은데..
 
+import sys
 
+input = sys.stdin.readline
+
+a = []
+for _ in range(int(input())):
+    a.append(int(input()))
+
+a.sort()
+for s in a:
+    print(s)
