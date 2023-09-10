ba29d9e9d09b67e0f0f25b16d3cc827f7f1010dc
ffe4el <codkan20@gmail.com>
2023-09-10 14:31:59 +0900

    1018 풀이 완료

---
+++ ALFS/2+/1018.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+string BW[8] = {
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB"
+};
+
+string WB[8] = {
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW"
+};
+
+string arr[50];
+int BW_cnt(int x, int y){
+    int cnt = 0; //초기화 필요
+    for(int i=0; i<8; i++){
+        for(int j=0; j<8; j++){
+            if(arr[x+i][y+j]!= BW[i][j]){
+                cnt++;
+            }
+        }
+    }
+    return cnt;
+}
+int WB_cnt(int x, int y){
+    int cnt = 0; //초기화 필요
+    for(int i=0; i<8; i++){
+        for(int j=0; j<8; j++){
+            if(arr[x+i][y+j] != WB[i][j]){
+                cnt++;
+            }
+        }
+    }
+    return cnt;
+}
+
+int main(){
+    int m, n;
+    
+    cin >> m >> n; 
+
+
+    for(int i=0; i<m; i++){
+        cin >> arr[i];
+    }
+    
+    int r_min = 10000;
+    for(int i=0; i+8<=m; i++){
+        for(int j=0; j+8<=n; j++){
+            int tmp;
+            tmp=min(BW_cnt(i,j),WB_cnt(i,j));
+            if(tmp < r_min){
+                r_min = tmp;
+            }
+        }
+    
+    }
+
+    cout << r_min;
+
+    return 0;
+}
