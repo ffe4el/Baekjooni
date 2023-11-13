e32c662902622ac46dad7f6e0c176b070e80e500
ffe4el <codkan20@gmail.com>
2023-11-14 00:03:12 +0900

    이진탐색 완료

--- 이코테/test.py
+++ 이코테/test.py
@@ -1,11 +1,24 @@
-n,m = map(int, input().split())
+def binary_search(arr, target, start, end):
+    mid = (start + end) // 2
+    if arr[mid] == target:
+        return mid
+    elif arr[mid] < target:
+        return binary_search(arr, target, mid+1, end)
+    else :
+        return binary_search(arr, target, start, mid-1)
 
-arr = [[0] * m] * n
 
-for i in range(n):
-    arr[i] = int(input())
+n = int(input())
+a = list(map(int, input().split()))
+a.sort() #사전 정렬
 
-for i in range(n):
-    for j in range(m):
-        print(arr[i][j])
-    print("\n")
+m = int(input())
+b = list(map(int, input().split()))
+b.sort() #사전 정렬
+
+for i in range(len(b)):
+    if binary_search(a, b[i], 0, len(a)-1):
+        print("yes", end=' ')
+    else :
+        print("no", end=' ')
+
